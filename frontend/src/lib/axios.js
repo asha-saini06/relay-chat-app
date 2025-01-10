8378339590c42bf065390ce424664eff5ff68926
Asha Saini <sainiasha.ash@gmail.com>
2025-01-11 00:22:36 +0530

    Refactor project structure & update configurations for relay-chat-app integration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
     withCredentials: true,
-});
+}); // This is how we can use the same code for both development and production environments. If we're in development, we'll use the localhost URL. If we're in production, we'll use the relative URL.
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; // This is how we can use the same code for both development and production environments. If we're in development, we'll use the localhost URL. If we're in production, we'll use the relative URL.
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
